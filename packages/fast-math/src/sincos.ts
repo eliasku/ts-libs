bd62681f3d8676e739d5dd7d7d807f69e0b9f1b3
Ilya <ilyak@playtika.com>
2020-09-03 15:08:36 +0300

    fix(fast-math): use PI_2 const

--- packages/fast-math/src/sincos.ts
+++ packages/fast-math/src/sincos.ts
@@ -1,9 +1,10 @@
 const SIN_TABLE_SIZE = 0x10000;
-const SIN_TABLE_MASK = 0xffff;
-const SIN_STEP = SIN_TABLE_SIZE / (2.0 * Math.PI);
-const COS_SHIFT = SIN_TABLE_SIZE >>> 2;
-const INV_SIN_STEP = (2.0 * Math.PI) / SIN_TABLE_SIZE;
 const SIN_TABLE = new Float32Array(SIN_TABLE_SIZE);
+const SIN_TABLE_MASK = 0xffff;
+const PI_2 = 2.0 * Math.PI;
+const SIN_STEP = SIN_TABLE_SIZE / PI_2;
+const COS_SHIFT = SIN_TABLE_SIZE >>> 2;
+const INV_SIN_STEP = PI_2 / SIN_TABLE_SIZE;
 
 for (let i = 0; i < SIN_TABLE_SIZE; ++i) {
   SIN_TABLE[i] = Math.sin(INV_SIN_STEP * i);
