ecffed0e522cbf263f8f7348e6f45a4a584c2507
Ilya <ilyak@playtika.com>
2020-09-03 16:04:27 +0300

    fix(fast-math): constant names

--- packages/fast-math/src/acos.ts
+++ packages/fast-math/src/acos.ts
@@ -1,10 +1,10 @@
-const VALUES_SIZE = 0x10001;
-const VALUES_COUNT = 0x10000; // include 0 symmetric
-const INV_ACOS_STEP = 2.0 / VALUES_COUNT;
-const ACOS_STEP = VALUES_COUNT >>> 1;
-const ACOS_TABLE = new Float32Array(VALUES_SIZE);
+const ACOS_TABLE_SIZE = 0x10001;
+const ACOS_TABLE_MAX = 0x10000; // include 0 symmetric
+const INV_ACOS_STEP = 2.0 / ACOS_TABLE_MAX;
+const ACOS_STEP = ACOS_TABLE_MAX >>> 1;
+const ACOS_TABLE = new Float32Array(ACOS_TABLE_SIZE);
 
-for (let i = 0; i < VALUES_SIZE; ++i) {
+for (let i = 0; i < ACOS_TABLE_SIZE; ++i) {
   ACOS_TABLE[i] = Math.acos(-1.0 + INV_ACOS_STEP * i);
 }
 
