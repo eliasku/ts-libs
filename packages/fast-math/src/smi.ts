ba92e0f83f4834c77a6ad7234e5dfe049fc3c004
Ilya <ilyak@playtika.com>
2020-09-03 11:03:56 +0300

    feat(fast-math): disable performance test

--- packages/fast-math/src/smi.ts
+++ packages/fast-math/src/smi.ts
@@ -1,15 +1,17 @@
-const assert = (cond: unknown, message: string) => {
-  if (!cond) throw new Error(message);
-};
-
-const isSMI = (x: number) => Number.isInteger(x) && x >= -(2 ** 30) && x < 2 ** 30;
+// const assert = (cond: unknown, message: string) => {
+//   if (!cond) throw new Error(message);
+// };
+//
+// const isSMI = (x: number) => Number.isInteger(x) && x >= -(2 ** 30) && x < 2 ** 30;
 
 export const absSMI = (x: number): number => {
-  process.env.DEBUG && assert(isSMI(x), 'not SMI');
+  // TODO: runtime type-checking
+  /* process.env.DEBUG && assert(isSMI(x), 'not SMI'); */
   return x < 0 ? -x : x;
 };
 
 export const signSMI = (x: number): -1 | 0 | 1 => {
-  process.env.DEBUG && assert(isSMI(x), 'not SMI');
+  // TODO: runtime type-checking
+  /* process.env.DEBUG && assert(isSMI(x), 'not SMI'); */
   return x ? (x < 0 ? -1 : 1) : 0;
 };
